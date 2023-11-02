10ce1b1f9dfb7b5c26fddafe1580b33718e17ced
ivansanguezax <ivansanguezax@gmail.com>
2023-11-02 15:30:55 -0400

    [005-Login&Logout]

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -26,4 +26,31 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+    const ismatch = await bcrypt.compare(password, userFound.password);
+    if (!ismatch) return res.status(400).json({ message: "Wrong password" });
+
+
+    
+    const token = await createAccessToken({ id: userFound._id });
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const logout = async (req, res) => {
+    res.cookie("token", "", { expires: new Date(0) });
+    return res.send(200);
+}
